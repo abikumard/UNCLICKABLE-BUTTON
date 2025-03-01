0a444d8a62d813fdc0c41928dd6da03a7df54da9
Abi Kumar <146969373+abikumard@users.noreply.github.com>
2025-03-01 12:18:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const wrapper = document.querySelector('.wrapper');
+const question = document.querySelector('.question');
+const yesBtn = document.querySelector('.yes-btn');
+const noBtn = document.querySelector('.no-btn');
+const wrapperRect = wrapper.getBoundingClientRect();
+const noBtnRect = noBtn.getBoundingClientRect();
+const image=document.getElementById("image3")
+yesBtn.addEventListener('click', () => {
+    question.innerHTML = 'I Love You Too 🥰 :)';
+    image.style.display="block";
+    
+});
+noBtn.addEventListener('mouseover', () => {
+    const i = Math.floor(Math.random() * (wrapperRect.width - noBtnRect.width)) + 1;
+    const j = Math.floor(Math.random() * (wrapperRect.height - noBtnRect.height)) + 1;
+    noBtn.style.left = i + 'px';
+    noBtn.style.top = j + 'px';
+});
+
